e2f12eded2212aadd385bad7bfcaa456b7d32f62
Tim Davidson <timdavidson2@gmail.com>
2021-08-20 13:44:29 -0400

    Update server.js with dotenv information

--- server.js
+++ server.js
@@ -2,3 +2,21 @@ const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const conTable = require("console.table");
 
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    process.env.HOST,
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+  
+    console.log(`Connected to the employees database.`)
+);
